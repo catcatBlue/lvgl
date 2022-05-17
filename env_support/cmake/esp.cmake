6546429cd31196a0aff0ab33469cb8711e2bea0e
catcatBlue <78634778+catcatBlue@users.noreply.github.com>
2022-05-17 15:21:34 +0800

    包含demos

--- env_support/cmake/esp.cmake
+++ env_support/cmake/esp.cmake
@@ -1,4 +1,4 @@
-file(GLOB_RECURSE SOURCES ${LVGL_ROOT_DIR}/src/*.c)
+file(GLOB_RECURSE SOURCES ${LVGL_ROOT_DIR}/src/*.c ${LVGL_ROOT_DIR}/demos/*.c)
 
 idf_build_get_property(LV_MICROPYTHON LV_MICROPYTHON)
 
@@ -22,7 +22,7 @@ if(LV_MICROPYTHON)
   endif()
 else()
   idf_component_register(SRCS ${SOURCES} INCLUDE_DIRS ${LVGL_ROOT_DIR}
-                         ${LVGL_ROOT_DIR}/src ${LVGL_ROOT_DIR}/../)
+                         ${LVGL_ROOT_DIR}/src ${LVGL_ROOT_DIR}/../ ${LVGL_ROOT_DIR}/demos/)
 
   target_compile_definitions(${COMPONENT_LIB} PUBLIC "-DLV_CONF_INCLUDE_SIMPLE")
 
